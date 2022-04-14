35bdab71fa9d7f35677afb1133f53e9f25b869d3
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2022-04-14 19:46:55 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,8 @@ export default function Home() {
   return (
     <>
       <section className='container'>
-        <h1>Heydfgdfgdf</h1>
-        <h1>Hey</h1>
-        <h1>Hey</h1>
-        <h1>Hey</h1>
-        <h1>Hey</h1>
-        <h1>Hey</h1>
-        <h1>Hey34</h1>
-        <h1>Hey</h1>
-        <h1>Hey</h1>
-        <h1>Hey</h1>
-       
+        <h1>Hey Mark</h1>
+
     
 
       </section>
